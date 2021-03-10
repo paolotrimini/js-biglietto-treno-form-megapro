a470ff1ea72a685764247b1ae5f4d3c4f384381b
paolotrimini <paolo.trimini@gmail.com>
2021-03-10 20:34:56 +0100

    inserimento dati tabella

--- js/script.js
+++ js/script.js
@@ -10,32 +10,53 @@ generaBtn.addEventListener('click', function(){
     var name = document.getElementById('name').value;
     // console.log(name);
     // console.log(document.getElementById('name'));
+
     var distanza = parseInt(document.getElementById('distance').value);
     var eta = document.getElementById('eta').value;
-    console.log(name, distanza, eta);
-
+    //console.log(name, distanza, eta);
 
     if (name !== '' && distanza > 0 && eta !== ''){
         costoTotale = costo * distanza;
         // blocco else if per sconto
         console.log(costoTotale);
-        if (eta == 1 ){
+        if (eta == 1 ){     // minorenne
             // console.log('minorenne', eta);
+
             var sconto = costoTotale * 0.2;
             costoTotale = costoTotale - sconto;
-        } else if (eta == 3){
+            document.getElementById('sconto').innerHTML = 'Sconto <br> minorenne';
+
+        } else if (eta == 3){ // over 65
             // console.log('over65', eta);
+
             var sconto = costoTotale * 0.4;
             costoTotale = costoTotale - sconto;
+            document.getElementById('sconto').innerHTML = 'Sconto <br> Over 65';
         }
-        document.getElementById('costo-totale').innerHTML = costoTotale;
+
+        document.getElementById('costo-totale').innerHTML = costoTotale + ' &#8364;';
+
         var cp = Math.floor(Math.random() * (100000-90000)) + 90000;
         document.getElementById('codCp').innerHTML = cp;
-    } else{
+
+        var numCarrozza = Math.floor(Math.random() * 30 ) + 1; // num.carrozze 1 - 30
+        document.getElementById('carrozza').innerHTML = numCarrozza;
+
+        document.getElementById('nominativo').innerHTML = name;
+
+    } else{ // altre fasce età
         console.log('nah');
     }
 });
 
 annullaBtn.addEventListener('click', function(){
-    console.log('ho cliccato sul pulsante annulla')
+    //console.log('ho cliccato sul pulsante annulla')
+
+    document.getElementById('nominativo').innerHTML = '';
+    document.getElementById('sconto').innerHTML = '';
+    document.getElementById('carrozza').innerHTML = '';
+    document.getElementById('codCp').innerHTML = '';
+    document.getElementById('costo-totale').innerHTML = '';
+
+
 });
